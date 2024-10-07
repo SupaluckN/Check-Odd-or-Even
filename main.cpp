cfaa2e3df2ddc4d24bce82fb0ed438f58f4f4d3c
Supaluck Singjan <supalucksingjan@gmail.com>
2024-10-07 22:30:31 +0700

    Add basic

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+
+    /* check odd or even */
+
+    int number = 0;
+
+    while (number < 10) {
+
+        cout << "Number = " << number << endl;
+        number = number + 1;
+
+    }
+
+    return 0;
+}
